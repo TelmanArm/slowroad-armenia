61435a9f0545aa318f906e3837e0ffe710613cc7
Telman Gurgenyan <tgurgenyan@gmail.com>
2026-09-17 16:17:52 +0400

    Adde CRUD for place.

--- SlowRoad/Models/Place.cs
+++ SlowRoad/Models/Place.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SlowRoad.Models;
 
 public class Place
 {
-    public int Id { get; set; }                    
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(120)]
     public string Name { get; set; } = "";
-    public string? Region { get; set; }            
+
+    [StringLength(80)]
+    public string? Region { get; set; }
+
+    [StringLength(2000)]
     public string? Description { get; set; }
 }
